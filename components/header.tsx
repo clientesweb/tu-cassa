b93180900c08c199be80805972fbf5c2ec0a0fba
clientesweb <alexisifran32@gmail.com>
2025-09-25 02:46:56 -0300

    Actualizar header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,7 @@ export function Header() {
                   <NavigationMenuContent>
                     <div className="grid gap-3 p-6 w-[200px] bg-white">
                       <a
-                        href="#tradicional"
+                        href="/viviendas-tradicionales"
                         className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                       >
                         <div className="text-sm font-medium leading-none">Tradicional</div>
